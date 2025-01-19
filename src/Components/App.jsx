c6d230908f438e7204d596898da374023c055313
Ali Houmani <houmani.datalu@gmail.com>
2025-01-19 20:49:38 +0200

    Initiate

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to My Simple React App!</h1>
+            <p>This is a minimal setup.</p>
+        </div>
+    );
+}
+
+export default App;
